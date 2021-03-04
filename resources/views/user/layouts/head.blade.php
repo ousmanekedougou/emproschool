888a59acdb6c6d1d13c6c1e8f23bf43b5397e4fc
ousmanekedougou <yabaye075@gmail.com>
2021-03-04 00:45:47 +0000

    le fave_icone

--- resources/views/user/layouts/head.blade.php
+++ resources/views/user/layouts/head.blade.php
@@ -1,7 +1,7 @@
 <meta charset="UTF-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1.0,maximum-scale=1">
-
+<link rel="shortcut icon" href="{{asset('user/images/logo-empro.png')}}">
 <title>EMPRO</title>
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.min.css">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick-theme.min.css">
