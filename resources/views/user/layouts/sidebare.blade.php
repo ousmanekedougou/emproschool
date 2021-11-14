377b6cd3a47a7c300fae553051beb8875d2c75a2
ousmanekedougou <yabaye075@gmail.com>
2021-11-14 00:01:07 +0000

    commit 2

--- resources/views/user/layouts/sidebare.blade.php
+++ resources/views/user/layouts/sidebare.blade.php
@@ -3,21 +3,21 @@
     <div class="form-header">
       <a href="/" class="logo"><img style="width: 100% auto;" src="{{ asset('user/images/logo-empro.png') }}" alt=""></a>
       <p style="color:#000;font-weight:800;">
-        L'école de l'innovation et de l'expertise informatique 
+        L'école de l'innovation et de l'éxpértise informatique 
       </p>
     </div>
  
 
     <div class="form-content" >
   
-      <h3 class="h3">Nos offres de Services</h3>
+      <h3 class="h3">Nos offres de Sérvices</h3>
 
       <div class="service">
      
 
         <div class="service_icon text-center service_intern" >
           <img style="width:25%;" src="{{ asset('user/images/d.png') }}" alt="">
-          <h4 style="text-align: center;color:#394d6e;font-weight:800">Developpement web</h4>
+          <h4 style="text-align: center;color:#394d6e;font-weight:800">Développement web</h4>
           <p class="text-center">
             Vous avez un projet web , une plateforme quelconque notre service est a votre disposition
           </p>
